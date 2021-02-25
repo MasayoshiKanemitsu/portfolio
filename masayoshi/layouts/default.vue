dfaf22458cc1ff63f439fec52928087df6a8177f
masayoshi <>
2021-02-26 00:02:12 +0900

    nuxt

---
+++ masayoshi/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ masayoshi/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="test">
+    <p>Hello World</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+.test {
+  p {
+    color: red;
+    margin: 0;
+  }
+}
+</style>
